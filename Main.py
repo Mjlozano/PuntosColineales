8add7d79d365937590d129e036849bea12df8c24
ApoloJ <mjlozanoh98@gmail.com>
2020-10-16 17:08:30 -0500

    Se agregó el archivo "requeriments" y se quitaron importaciones innecesarias

--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
 from Colineal import Colineal
 from Colineal import SegmentoDeLinea
-from Punto import Punto
 
 fileName = input("Ingrese nombre del archivo>>>  ")
 
