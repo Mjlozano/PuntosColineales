0a9514ab4714f626d951936045603cfcaa4b75f5
ApoloJ <mjlozanoh98@gmail.com>
2020-10-17 16:35:30 -0500

    cambios menores

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Colineal import Colineal
 
-fileName = input("Ingrese nombre del archivo>>>  ")
+print("_____________________________________________")
+fileName = input("Ingrese nombre del archivo:  ")
 
 # Se manda el nombre del archivo para poder ser manipulado
 puntosColineales = Colineal(fileName)
