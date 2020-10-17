abd1725ee6d0819465e00362ece41599cfb5c392
ApoloJ <mjlozanoh98@gmail.com>
2020-10-17 16:52:20 -0500

    se agregó un metodo para limpiar la consola

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
 from Colineal import Colineal
+import os
 
-print("_____________________________________________\n")
+
+# Este metodo es para limpiar la consola y asi limpiar
+# el texto de la ejecución anterior
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+cls()
+
 fileName = input("Ingrese nombre del archivo:  ")
 
 # Se manda el nombre del archivo para poder ser manipulado
